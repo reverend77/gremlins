c323f680d9429ab5506ea8886491c78b925a1cc4
Lukasz Marek <marek.lucas@gmail.com>
2018-07-21 16:24:35 +0200

    added client code

--- gremlins/client/client_main.py
+++ gremlins/client/client_main.py
@@ -1,1 +1,14 @@
-#logic for client main
+from gremlins.common.task_distribution import TaskSubscriber
+import pika
+from multiprocessing import Process
+
+
+def start_subscriber():
+    connection2 = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+    subscriber = TaskSubscriber(connection2)
+    subscriber.start()
+
+
+if __name__ == "__main__":
+    subscriber_proc = [Process(target=start_subscriber) for __ in range(4)]
+    [proc.start() for proc in subscriber_proc]
