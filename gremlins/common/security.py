8a38f3f14009c2caaee593a11a70cbb841f79ac2
Lukasz Marek <marek.lucas@gmail.com>
2018-09-15 22:46:53 +0200

    created dummy encoder and decoder

---
+++ gremlins/common/security.py
@@ -0,0 +1,15 @@
+class DataEncoder:
+    def __init__(self):
+        pass
+
+    def encode(self, data):
+        return data
+
+
+class DataDecoder:
+    def __init__(self):
+        pass
+
+    def decode(self, data):
+        return data
+
